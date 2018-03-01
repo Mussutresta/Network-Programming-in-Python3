31abbe3c6face076db648595ecdbb8d4ec8423d3
Shifa Samreen <shifasamreen24@gmail.com>
2018-03-01 11:25:28 +0530

    The complete first file, which provides an introduction to socket programming in python

--- socketProg.py
+++ socketProg.py
@@ -9,7 +9,7 @@ print(s)
 
 # To communicate with a server, let us say a remote server
 
-server ="pythonpragramming.net"
+server ="google.com"
 
 #Next, specify what port we want to communicate on, 80 for http
 
@@ -18,4 +18,40 @@ port=80
 #You can also communicate using server's IP address
 
 server_ip= socket.gethostbyname(server)
+
+print(server_ip)
+
+#make request
+
+request= "GET / HTTP/1.1\n" + server+ "\n"
+
+#Now connect the server and the prot
+
+s.connect((server, port))
+
+#In Python3, the data is in Byte string format that is the incorrect format, so we need to encode. This 
+#isn't a problem in Python2.7
+
+s.send(request.encode())
+
+#1024 and 4096 etc is the buffer, that is how much data do we need to store at a time
+result=s.recv(4096)
+
+#print(result)
+
+#The result will have the srever, port, server ip address, the request that we made which starts with a 'b':denoting byte code 
+
+#else you could buffer and not print everythin al at once. 
+
+while(len(result)>0):
+	print(result)
+	result=s.recv(1024)
+
+
+
+
+
+
+
+
 
